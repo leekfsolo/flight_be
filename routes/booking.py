bc710500db6d366942c4b4c2b0911f5f7bd4eadc
leekfsolo <gialap2002@gmail.com>
2023-11-11 11:27:50 +0700

    feat: integrate paypal and GHN

--- routes/booking.py
+++ routes/booking.py
@@ -1,51 +1,98 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Depends
 from schemas.user import serializeDict, serializeList
 from models.booking import Booking
 from config.db import db
 from bson import ObjectId
+from provider.jwtProvider import jwtBearer
 from provider.authProvider import get_userId_from_request
+from routes.ticket import get_ticket_detail
 import datetime
+from decouple import config
+import httpx
+import json
+from .cart import handle_response
 
+GHN_TOKEN = config('GHN_TOKEN')
+GHN_SHOP_ID = config('GHN_SHOP_ID')
+GHN_BASE_URL = "https://dev-online-gateway.ghn.vn/shiip/public-api/v2"
 booking = APIRouter()
+
 
 @booking.get('')
 async def get_all_booking(Authorization: str = Header(default=None)):
-  userId = get_userId_from_request(Authorization)
-  bookings = serializeList(db.booking.aggregate([{
-            '$match': {
-              'userId': userId,
-            }  
-          },{
-            '$lookup': {
-                'from': 'tickets', 
-                'localField': 'ticketId', 
-                'foreignField': '_id', 
-                'as': 'items'
-            }
-        }]))
-  bookingData = []
-  for booking in bookings:
-    booking['ticketId'] = str(booking['ticketId'])
-    booking.pop('userId')
-    booking['items'] = [serializeDict(item) for item in booking['items']][0]
-    bookingData.append(booking)
-    
-  return {'success': True, 'data': bookingData}
+    userId = get_userId_from_request(Authorization)
+    bookings = serializeList(db.booking.aggregate([{
+        '$match': {
+            'userId': userId,
+        }
+    }, {
+        '$lookup': {
+            'from': 'tickets',
+            'localField': 'ticketId',
+            'foreignField': '_id',
+            'as': 'items'
+        }
+    }]))
+    bookingData = []
+    for booking in bookings:
+        booking['ticketId'] = str(booking['ticketId'])
+        booking.pop('userId')
+        booking['items'] = [serializeDict(item)
+                            for item in booking['items']][0]
+        bookingData.append(booking)
+
+    return {'success': True, 'data': bookingData}
+
 
 @booking.get('/{id}')
 async def get_booking_detail(id):
-  return serializeDict(db.booking.find_one({'_id': ObjectId(id)}))
+    return serializeDict(db.booking.find_one({'_id': ObjectId(id)}))
 
-@booking.post('')
+
+@booking.post('', dependencies=[Depends(jwtBearer())])
 async def add_booking(booking: Booking, Authorization: str = Header(default=None)):
-  userId = get_userId_from_request(Authorization)
-  bookingData = dict(booking)
-  bookingData['ticketId'] = ObjectId(booking.ticketId)
-  bookingData['userId'] = userId
-  bookingData['created_at'] = datetime.datetime.now()
-  db.booking.insert_one(bookingData)
-  return {'success': True, 'message': 'Booking added successfully'}
+    try:
+        userId = get_userId_from_request(Authorization)
+        bookingData = dict(booking)
+        bookingData['ticketId'] = ObjectId(booking.ticketId)
+        bookingData['userId'] = userId
+        bookingData['created_at'] = datetime.datetime.now()
+        db.booking.insert_one(bookingData)
+
+        ticketData = await get_ticket_detail(booking.ticketId)
+
+        async with httpx.AsyncClient(base_url=GHN_BASE_URL, headers={"ShopId": GHN_SHOP_ID, "Token": GHN_TOKEN, "Content-Type": "application/json"}) as client:
+            data = {
+                "required_note": "KHONGCHOXEMHANG",
+                "payment_type_id": 2,
+                "to_name": booking.email,
+                "to_phone": booking.phone,
+                "to_address": "268 Lý Thường Kiệt, Phường 14, Quận 10, Hồ Chí Minh, Vietnam",
+                "to_ward_code": "21311",
+                "to_district_id": 1461,
+                "service_id": 100039,
+                "service_type_id": 2,
+                "weight": 200,
+                "length": 20,
+                "width": 5,
+                "height": 2,
+                "items": [
+                    {
+                        "name": ticketData["flightId"],
+                        "code": booking.ticketId,
+                        "quantity": 1,
+                        "price": int(ticketData["price"]),
+                        "weight": 200,
+                    }
+                ]
+            }
+            await client.post("/shipping-order/create", data=json.dumps(data))
+        return {"success": True,
+                "message": "Booking added successfully"}
+    except Exception as error:
+        print("Failed to create order:", error)
+
 
 @booking.delete('/{id}')
 async def delete_booking(id):
-  return serializeDict(db.booking.find_one_and_delete({'_id': ObjectId(id)}))
+    return serializeDict(db.booking.find_one_and_delete({'_id': ObjectId(id)}))
--- routes/cart.py
+++ routes/cart.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from fastapi import APIRouter, Header, Depends, Body
 from schemas.user import serializeDict, serializeList
 from config.db import db
@@ -5,94 +6,176 @@ from bson import ObjectId
 from provider.authProvider import get_userId_from_request
 from provider.jwtProvider import jwtBearer
 import datetime
+import base64
+from decouple import config
+import httpx
+from models.CustomBaseModels import CustomBaseModel
+
+PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
+PAYPAL_CLIENT_SECRET = config('PAYPAL_CLIENT_SECRET')
+PAYPAL_BASE_URL = "https://api-m.sandbox.paypal.com"
+
+
+class PaypalApprove(CustomBaseModel):
+    orderId: str = Field(default=None)
+
+
+async def handle_response(response, success=True, message=""):
+    try:
+        json_response = await response.json()
+        return {
+            "jsonResponse": json_response,
+            "httpStatusCode": response.status_code,
+            "success": success,
+            "message": message
+        }
+    except Exception as err:
+        error_message = await response.text()
+        raise Exception(error_message)
+
+
+async def generate_access_token():
+    try:
+        if not PAYPAL_CLIENT_ID or not PAYPAL_CLIENT_SECRET:
+            raise Exception("MISSING_API_CREDENTIALS")
+
+        auth = base64.b64encode(
+            f"{PAYPAL_CLIENT_ID}:{PAYPAL_CLIENT_SECRET}".encode()).decode()
+
+        async with httpx.AsyncClient(base_url=PAYPAL_BASE_URL) as client:
+            response = await client.post("/v1/oauth2/token", data="grant_type=client_credentials", headers={
+                "Authorization": f"Basic {auth}"
+            })
+
+        data = response.json()
+        return data["access_token"]
+    except Exception as error:
+        print("Failed to generate Access Token:", error)
 
 cart = APIRouter()
 
+
 @cart.get('', dependencies=[Depends(jwtBearer())])
 async def get_all_cart_items(Authorization: str = Header(default=None)):
-  try:
-    userId = get_userId_from_request(Authorization)
-    cartList = serializeList(db.cart.aggregate([
+    try:
+        userId = get_userId_from_request(Authorization)
+        cartList = serializeList(db.cart.aggregate([
+            {
+                '$lookup': {
+                    'from': 'tickets',
+                    'localField': 'ticketId',
+                    'foreignField': '_id',
+                    'as': 'items'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'user',
+                    'localField': 'userId',
+                    'foreignField': '_id',
+                    'as': 'users'
+                }
+            }
+        ]))
+
+        myCartList = []
+        for cart in cartList:
+            if str(cart['userId']) == str(userId):
+                for item in cart['items']:
+                    item['id'] = str(item['_id'])
+                    item.pop('_id')
+                    myCartList.append(item)
+
+        return {'success': True, 'data': myCartList}
+    except:
+        return {'success': False}
+
+
+@cart.get('/{id}', dependencies=[Depends(jwtBearer())])
+async def get_cart_item(id: str):
+    cartItem = serializeList(db.cart.aggregate([
+        {
+            '$match': {
+                '_id': ObjectId(id),
+            }
+        },
+        {
+            '$sort': {
+                'created_at': -1
+            }
+        },
         {
             '$lookup': {
-                'from': 'tickets', 
-                'localField': 'ticketId', 
-                'foreignField': '_id', 
+                'from': 'tickets',
+                'localField': 'ticketId',
+                'foreignField': '_id',
                 'as': 'items'
             }
-        }, {
-            '$lookup': {
-                'from': 'user', 
-                'localField': 'userId', 
-                'foreignField': '_id', 
-                'as': 'users'
-            }
         }
     ]))
-    
-    myCartList = []
-    for cart in cartList:
-      if str(cart['userId']) == str(userId):
-        for item in cart['items']:
-          item['id'] = str(item['_id'])
-          item.pop('_id')
-          myCartList.append(item)
-          
-    return {'success': True, 'data': myCartList}
-  except:
-    return {'success': False}
-  
-@cart.get('/{id}', dependencies=[Depends(jwtBearer())])
-async def get_cart_item(id: str, Authorization: str = Header(default=None)):
-  cartItem = serializeList(db.cart.aggregate([
-      {
-        '$match': {
-          '_id': ObjectId(id),
-        }  
-      },
-      {
-        '$sort': {
-          'created_at': -1
-        }  
-      },
-      {
-        '$lookup': {
-            'from': 'tickets', 
-            'localField': 'ticketId', 
-            'foreignField': '_id', 
-            'as': 'items'
-        }
-      }
-    ]))
-  
-  
-  currentCartItem = cartItem[0]['items'][0]
-  currentCartItem['id'] = str(currentCartItem['_id'])
-  currentCartItem.pop('_id')
-        
-  return currentCartItem
+
+    currentCartItem = cartItem[0]['items'][0]
+    currentCartItem['id'] = str(currentCartItem['_id'])
+    currentCartItem.pop('_id')
+
+    return currentCartItem
+
 
 @cart.post('', dependencies=[Depends(jwtBearer())])
 async def add_cart_item(ticketId: str, Authorization: str = Header(default=None)):
-  try:
-    userId = get_userId_from_request(Authorization)
-    id = ObjectId()
-    
-    ticket = {
-      '_id': id,
-      'userId': userId,
-      'ticketId': ObjectId(ticketId),
-      'created_at': datetime.datetime.now(),
-    }
-    
-    db.cart.insert_one(dict(ticket))
-    
-    cartItem = await get_cart_item(str(id), Authorization)
-    
-    return {'success': True, 'message': 'add ticket successfully', 'data': cartItem}
-  except:
-    return {'success': False}
+    try:
+        userId = get_userId_from_request(Authorization)
+        id = ObjectId()
+
+        ticket = {
+            '_id': id,
+            'userId': userId,
+            'ticketId': ObjectId(ticketId),
+            'created_at': datetime.datetime.now(),
+        }
+
+        db.cart.insert_one(dict(ticket))
+
+        cartItem = await get_cart_item(str(id))
+
+        return {'success': True, 'message': 'add ticket successfully', 'data': cartItem}
+    except:
+        return {'success': False}
+
 
 @cart.delete('/{id}')
 async def delete_cart(id):
-  return serializeDict(db.cart.find_one_and_delete({'_id': ObjectId(id)}))
+    return serializeDict(db.cart.find_one_and_delete({'_id': ObjectId(id)}))
+
+
+@cart.post('/paypal/create-order', dependencies=[Depends(jwtBearer())])
+async def createPaypalOrder(order: object = Body(default=None)):
+    try:
+        accessToken = await generate_access_token()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {accessToken}",
+        }
+
+        async with httpx.AsyncClient(base_url=PAYPAL_BASE_URL, headers=headers) as client:
+            response = await client.post("/v2/checkout/orders", json=order)
+
+            return response.json()
+    except:
+        return {'success': False}
+
+
+@cart.post('/paypal/approve-order', dependencies=[Depends(jwtBearer())])
+async def createPaypalOrder(data: PaypalApprove):
+    try:
+        accessToken = await generate_access_token()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {accessToken}",
+        }
+
+        async with httpx.AsyncClient(base_url=PAYPAL_BASE_URL, headers=headers) as client:
+            response = await client.post(f"/v2/checkout/orders/{data.orderId}/capture")
+
+            return response.json()
+    except:
+        return {'success': False}
